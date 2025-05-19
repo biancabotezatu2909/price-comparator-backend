fd5a6587599a6ed833c5eb9e1e2298974868ad33
Bianca Botezatu <bianca.botezatu@stud.ubbcluj.ro>
2025-05-19 15:46:51 +0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/pricecomparator/price_comparator_backend/PriceComparatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pricecomparator.price_comparator_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceComparatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceComparatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pricecomparator/price_comparator_backend/PriceComparatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pricecomparator.price_comparator_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceComparatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
