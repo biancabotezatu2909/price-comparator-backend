3e4f85c4da7a1342f3679b7d53f564f3b51d1a66
Bianca Botezatu <bianca.botezatu@stud.ubbcluj.ro>
2025-05-19 16:16:40 +0300

    chore: create clean project package structure

---
+++ src/main/java/com/pricecomparator/price_comparator_backend/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.pricecomparator.price_comparator_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Swagger!";
+    }
+}
