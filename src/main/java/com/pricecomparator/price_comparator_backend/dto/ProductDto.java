af7be13c21a8c005d2ccfdf2e31ebfee39a2c0f7
Bianca Botezatu <bianca.botezatu@stud.ubbcluj.ro>
2025-05-23 09:56:07 +0300

    added product dto to protect internal schema

---
+++ src/main/java/com/pricecomparator/price_comparator_backend/dto/ProductDto.java
@@ -0,0 +1,37 @@
+package com.pricecomparator.price_comparator_backend.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDate;
+
+public record ProductDto(
+        @Schema(description = "Business ID of the product", example = "P001")
+        String productId,
+
+        @Schema(description = "Name of the product", example = "lapte zuzu")
+        String productName,
+
+        @Schema(description = "Product category", example = "lactate")
+        String productCategory,
+
+        @Schema(description = "Brand", example = "Zuzu")
+        String brand,
+
+        @Schema(description = "Quantity in package", example = "1.0")
+        double packageQuantity,
+
+        @Schema(description = "Measurement unit", example = "l")
+        String packageUnit,
+
+        @Schema(description = "Product price", example = "9.90")
+        double price,
+
+        @Schema(description = "Currency", example = "RON")
+        String currency,
+
+        @Schema(description = "Store selling the product", example = "Lidl")
+        String store,
+
+        @Schema(description = "Date when the price was recorded", example = "2025-05-08", type = "string", format = "date")
+        LocalDate date
+) {}
--- src/main/java/com/pricecomparator/price_comparator_backend/util/ProductCsvImporter.java
+++ src/main/java/com/pricecomparator/price_comparator_backend/util/ProductCsvImporter.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
 import java.util.*;
 
 @Component
@@ -64,7 +65,7 @@ public class ProductCsvImporter {
                                     .price(Double.parseDouble(fields[6]))
                                     .currency(fields[7])
                                     .store(store)
-                                    .date(date)
+                                    .date(LocalDate.parse(date))
                                     .build();
 
                             allProducts.add(product);
